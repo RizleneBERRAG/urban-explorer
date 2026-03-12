3f89a6b5b8c4126203f11c82ba2413967d1e5347
gama8speed <franck-joel.nzokou-takouju@estiam.com>
2026-03-12 14:16:00 +0100

    Correction de la casse du fichier MapScreen

--- src/screens/mapscreen.tsx
+++ src/screens/mapscreen.tsx
@@ -1,31 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react"
+import { StyleSheet, View } from "react-native"
+import MapView, { Marker } from "react-native-maps"
+import { Lieu } from "../types/lieu"
 
 export default function MapScreen() {
+  const [lieux, setLieux] = useState<Lieu[]>([])
+
+  useEffect(() => {
+  fetch(
+    "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-culturels-a-paris/records?limit=30"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // Adapter au vrai format de l'API
+      const lieuxAPI = data.records.map((item: any) => ({
+        nom_usuel: item.fields.nom_usuel,
+        adresse: item.fields.adresse,
+        coordonnees_geo: item.fields.coordonnees_geo,
+      }));
+      setLieux(lieuxAPI);
+    })
+    .catch((err) => console.log(err));
+}, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Carte</Text>
-      <Text style={styles.text}>
-        Ici on affichera la géolocalisation des lieux.
-      </Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 48.8566,
+          longitude: 2.3522,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+      >
+        {lieux.map((lieu, index) => {
+          if (!lieu.coordonnees_geo) return null
+          return (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: lieu.coordonnees_geo.lat,
+                longitude: lieu.coordonnees_geo.lon,
+              }}
+              title={lieu.nom_usuel}
+              description={lieu.adresse}
+            />
+          )
+        })}
+      </MapView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: '700',
-    marginBottom: 10,
+  map: {
+    flex: 1,
   },
-  text: {
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
+}); 
